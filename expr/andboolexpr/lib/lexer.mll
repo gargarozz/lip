5ef75e7242253a937cd3fc806c44e98c8bdc5eed
Nicola Mocci <nicolamocci.nm@gmail.com>
2025-10-23 16:05:13 +0200

    begin andboolexpr

---
+++ expr/andboolexpr/lib/lexer.mll
@@ -0,0 +1,20 @@
+{
+open Parser
+}
+
+let white = [' ' '\t']+
+
+rule read =
+  parse
+  | white { read lexbuf }  
+  | "true" { TRUE }
+  | "false" { FALSE }
+  | "(" { LPAREN }
+  | ")" { RPAREN }
+  | "if" { IF }
+  | "then" { THEN }
+  | "else" { ELSE }
+  | "not" { NOT }
+  | "and" { AND }
+  | "or" { OR }
+  | eof { EOF }
---
+++ expr/andboolexpr/lib/main.ml
@@ -0,0 +1,32 @@
+open Ast
+
+let rec string_of_boolexpr = function
+    True -> "True"
+  | False -> "False"
+  | If(e0,e1,e2) -> "If(" ^ (string_of_boolexpr e0) ^ "," ^ (string_of_boolexpr e1) ^ "," ^ (string_of_boolexpr e2) ^ ")"
+
+
+let parse (s : string) : boolExpr =
+  let lexbuf = Lexing.from_string s in
+  let ast = Parser.prog Lexer.read lexbuf in
+  ast
+
+
+exception NoRuleApplies
+
+let rec trace1 = function
+    If(True,e1,_) -> e1
+  | If(False,_,e2) -> e2
+  | If(e0,e1,e2) -> If(trace1 e0,e1,e2)
+  | _ -> raise NoRuleApplies
+
+let rec trace e = try
+    let e' = trace1 e
+    in e::(trace e')
+  with NoRuleApplies -> [e]
+
+
+let rec eval = function
+    True -> true
+  | False -> false
+  | If(e0,e1,e2) -> match eval e0 with true -> eval e1 | false -> eval e2
