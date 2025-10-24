d57d9b507e8b5566efc54e59a9851edd4c1ddc05
Nicola Mocci <nicolamocci.nm@gmail.com>
2025-10-24 17:32:32 +0200

    complete andboolexpr

--- expr/andboolexpr/lib/main.ml
+++ expr/andboolexpr/lib/main.ml
@@ -3,6 +3,9 @@ open Ast
 let rec string_of_boolexpr = function
     True -> "True"
   | False -> "False"
+  | Not(e) -> "Not(" ^ (string_of_boolexpr e) ^ ")"
+  | And(e0,e1) -> "And(" ^ (string_of_boolexpr e0) ^ "," ^ (string_of_boolexpr e1) ^ ")"
+  | Or(e0,e1) -> "Or(" ^ (string_of_boolexpr e0) ^ "," ^ (string_of_boolexpr e1) ^ ")"
   | If(e0,e1,e2) -> "If(" ^ (string_of_boolexpr e0) ^ "," ^ (string_of_boolexpr e1) ^ "," ^ (string_of_boolexpr e2) ^ ")"
 
 
@@ -17,7 +20,16 @@ exception NoRuleApplies
 let rec trace1 = function
     If(True,e1,_) -> e1
   | If(False,_,e2) -> e2
-  | If(e0,e1,e2) -> If(trace1 e0,e1,e2)
+  | If(e0,e1,e2) -> let e0' = trace1 e0 in If(e0',e1,e2)
+  | Not(True) -> False
+  | Not(False) -> True
+  | Not(e) -> let e' = trace1 e in Not(e')
+  | And(True,e) -> e
+  | And(False,_) -> False
+  | And(e1,e2) -> let e1' = trace1 e1 in And(e1',e2)
+  | Or(True,_) -> True
+  | Or(False,e) -> e
+  | Or(e1,e2) -> let e1' = trace1 e1 in Or(e1',e2)
   | _ -> raise NoRuleApplies
 
 let rec trace e = try
@@ -29,4 +41,7 @@ let rec trace e = try
 let rec eval = function
     True -> true
   | False -> false
+  | Not(e) -> not (eval e)
+  | And(e0,e1) -> (eval e0) && (eval e1)
+  | Or(e0,e1) -> (eval e0) || (eval e1)
   | If(e0,e1,e2) -> match eval e0 with true -> eval e1 | false -> eval e2
--- expr/andboolexpr/lib/parser.mly
+++ expr/andboolexpr/lib/parser.mly
@@ -14,6 +14,11 @@ open Ast
 %token OR
 %token EOF
 
+%nonassoc ELSE
+%left OR
+%left AND
+%right NOT
+
 %start <boolExpr> prog
 
 %%
@@ -27,6 +32,8 @@ expr:
   | FALSE { False }
   | IF; e1 = expr; THEN; e2 = expr; ELSE; e3 = expr; { If(e1, e2, e3) }
   | e1 = expr; AND; e2 = expr; { And(e1, e2) }
+  | NOT; e = expr; { Not(e) }
+  | e1 = expr; OR; e2 = expr; { Or(e1,e2) }
   | LPAREN; e=expr; RPAREN {e}
 ;
 
